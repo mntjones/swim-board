d203e3e39ada0a75e2a80577d21cf12a6740f643
Monica <mntamaru@gmail.com>
2019-07-07 00:38:04 -0700

    create module Api::V1 and namespace routes

--- swim-board-app/config/routes.rb
+++ swim-board-app/config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  resources :events
-  resources :swimmers
+
+	namespace :api do
+		namespace :v1 do
+  		resources :events
+  		resources :swimmers
+  	end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
